7d8dc2addb1a05e30a37a1ae18551297ae683a2f
Adnane EMBOUAZZA <adnaneembouazza77000@gmail.com>
2026-03-19 16:12:19 +0100

    ajout try catch controlleur suite

--- src/controllers/offrir.ts
+++ src/controllers/offrir.ts
@@ -2,34 +2,36 @@ import { Request, Response } from "express";
 import * as offrirService from "../services/offrir";
 
 export async function listAllOffre (req : Request , res : Response) : Promise<void> {
-    const offre = await offrirService.getAllOffre();
-    if(offre){
-        res.json(offre);
-    }
-    else{
-        res.status(404).json({ message: "Aucune offre trouvée" });
+    try {
+        const offre = await offrirService.getAllOffre();
+        if(offre){
+            res.json(offre);
+        }
+    }catch(error){
+        res.status(500).json({ message: "Erreur lors de la récupération des offres" });
     }
 };
 
 export async function listOffreByID (req : Request , res: Response) : Promise<void> {
     const {id} = req.params;
-    const offre = await offrirService.getOffreByID(id);
-    if(offre){
-        res.json(offre);
-    }
-    else{
-        res.status(404).json({ message: "Offre non trouvée" });
+    try {
+        const offre = await offrirService.getOffreByID(id);
+        if(offre){
+            res.json(offre);
+        }
+    }catch(error){
+        res.status(500).json({ message: "Erreur lors de la récupération de l'offre" });
     }
 };
 
 export async function createOffre(req : Request , res:Response) : Promise<void> {
     const data = req.body;
-    const offre = await offrirService.createOffre(data);
-
-    if(offre){
-        res.json(offre);
-    }
-    else{
+    try {
+        const offre = await offrirService.createOffre(data);
+        if(offre){
+            res.json(offre);
+        }
+    }catch(error){
         res.status(500).json({ message: "Erreur lors de la création de l'offre" });
     }
 };
@@ -37,24 +39,24 @@ export async function createOffre(req : Request , res:Response) : Promise<void> 
 export async function updateOffreByID(req: Request , res: Response) : Promise<void> {
     const {id} = req.params;
     const data = req.body;
-    const offre = await offrirService.updateOffreById(id , data);
-
-    if (offre){
-        res.json(offre);
-    }
-    else{
+    try{
+        const offre = await offrirService.updateOffreById(id , data);
+        if (offre){
+            res.json(offre);
+        }
+    }catch(error){
         res.status(500).json({ message: "Erreur lors de la mise à jour de l'offre" });
-    }
+}
 };
 
 export async function deleteOffreByID(req : Request , res: Response) : Promise<void> {
     const {id} = req.params;
-    const offre = await offrirService.deleteOffreByID(id);
-
-    if(offre){
-        res.json(offre);
-    }
-    else {
+    try {
+        const offre = await offrirService.deleteOffreByID(id);
+        if(offre){
+            res.json(offre);
+        }
+    }catch(error){
         res.status(500).json({ message: "Erreur lors de la suppression de l'offre" });
     }
 };
--- src/controllers/rapport.ts
+++ src/controllers/rapport.ts
@@ -2,60 +2,65 @@ import { Request, Response } from "express";
 import * as rapportService from "../services/rapport";
 
 export async function listAllRapport (req:Request , res : Response) : Promise<void> {
-    const rapport = await rapportService.getAllRapports();
-    if(rapport){
-        res.json(rapport);
-    }
-    else{
-        res.status(404).json({ message: "Aucun rapport trouvé" });
+    try{
+        const rapport = await rapportService.getAllRapports();
+        if(rapport){
+            res.json(rapport);
+        }
+    }catch(error){
+        res.status(500).json({ message: "Erreur lors de la récupération des rapports" });
     }
 };
 
 export async function listRapportByID(req : Request , res : Response) : Promise<void> {
     const {id} = req.params;
-    const rapport = await rapportService.getRapportByID(id);
+    try {
+        const rapport = await rapportService.getRapportByID(id);
 
-    if (rapport){
-        res.json(rapport);
-    }
-    else {
+        if (rapport){
+            res.json(rapport);
+        }
+    }catch(error){
         res.status(404).json({ message: "Rapport non trouvé" });
     }
 };
 
 export async function createRapport(req : Request , res : Response) : Promise<void> {
     const data = req.body;
-    const rapport = await rapportService.createRapport(data);
+    try {
+        const rapport = await rapportService.createRapport(data);
 
-    if (rapport){
-        res.json(rapport);
-    }
-    else {
+        if (rapport){
+            res.json(rapport);
+        }
+    }catch(error){
         res.status(500).json({ message: "Erreur lors de la création du rapport" });
     }
 };
 
 export async function updateRapportByID(req : Request , res : Response) : Promise<void> {
     const {id} = req.params;
-    const  data =req.body;
-    const rapport = await rapportService.updateRapportByID(id , data);
+    try {
+        const  data =req.body;
+        const rapport = await rapportService.updateRapportByID(id , data);
 
-    if (rapport){
-        res.json(rapport);
-    }
-    else {
+        if (rapport){
+            res.json(rapport);
+        }
+    }catch(error){
         res.status(500).json({ message: "Erreur lors de la mise à jour du rapport" });
     }
 };
 
 export async function deleteRapportByID(req: Request , res:Response) : Promise<void> {
     const {id} = req.params;
-    const rapport = await rapportService.deleteRapportByID(id);
+    try {
+        const rapport = await rapportService.deleteRapportByID(id);
 
-    if (rapport){
-        res.json(rapport);
-    }
-    else {
+        if (rapport){
+            res.json(rapport);
+        }
+    }catch(error){
         res.status(500).json({ message: "Erreur lors de la suppression du rapport" });
     }
 };
